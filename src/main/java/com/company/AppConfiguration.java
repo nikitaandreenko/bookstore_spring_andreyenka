2fdecd9fdace719055e8a45317922b23e5637e8e
nikitaandreenko <n.l.andreenko@gmail.com>
2022-08-21 13:25:53 +0300

    Switch slf4j

--- src/main/java/com/company/AppConfiguration.java
+++ src/main/java/com/company/AppConfiguration.java
@@ -6,7 +6,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
