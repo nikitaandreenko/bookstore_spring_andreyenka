f1b5430776f713cd2c5c551c3938db59380bc500
nikitaandreenko <n.l.andreenko@gmail.com>
2022-09-30 20:27:18 +0300

    Change log4j2.xml

--- src/main/java/com/company/service/impl/BookServiceImpl.java
+++ src/main/java/com/company/service/impl/BookServiceImpl.java
@@ -8,6 +8,8 @@ import com.company.service.dto.ObjectMapperService;
 import com.company.service.exception.EntityNotFoundException;
 import com.company.service.exception.ValidateException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
